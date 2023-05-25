36ad253922922b3c6194a082a91223c4341b1281
Victoria Kalyniuk <tekalyniuk@hotmail.com>
2023-05-25 22:15:12 +0000

    changes

---
+++ mapTest.js
@@ -0,0 +1,7 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+
+const nameLengths = lighthouses.map((x) => x.length);
+
+
+console.log(nameLengths);
+// write code here to use map
